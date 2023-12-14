2e0f7f2ca500431dcecc1e383df51bbf82a6bcbd
shivammishra349 <shivam9.mishra1999@gmail.com>
2023-12-14 14:42:40 +0530

    file commited

--- app.js
+++ app.js
@@ -11,5 +11,23 @@
 
 // GET ELEMENT BY TAG NAME   
 
-let li=document.getElementsByTagName('li');
-console.log(li)
+// let li=document.getElementsByTagName('li');
+// console.log(li).
+
+
+// let input=document.querySelector('input');
+// input.value='hello world';
+
+// let submit=document.querySelector('input[type="submit"]');
+// submit.value='send';
+// let secItem=document.querySelector('.list-group-item');
+// secItem.style .color='green'
+
+// let secitem=document.querySelector('.list-group-item:nth-child(2)');
+// secitem.style .color='red';    
+
+// let seconditem=document.querySelector('.list-group-item:last-child');
+// seconditem.style.color='blue';
+let item=document.getElementsByClassName('list-group-item');
+item[1].style.backgroundColor="green"
+item[2].style.visibility="hidden"
