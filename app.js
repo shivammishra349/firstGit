88bf633ff6c3d6c9a1999cf7847f6e283ec5679c
shivammishra349 <shivam9.mishra1999@gmail.com>
2023-12-14 09:47:31 +0530

    seciond commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// GET ELEMENT BY CLASS NAME
+
+// let items=document.getElementsByClassName('list-group-item');
+// items[2].style.backgroundColor="green"
+// for(let i=0;i<items.length;i++)
+// {
+//     items[i].style.backgroundColor='red'
+// }
+
+
+
+// GET ELEMENT BY TAG NAME   
+
+let li=document.getElementsByTagName('li');
+console.log(li)
